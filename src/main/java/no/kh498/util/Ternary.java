54209004e20dbd015cd810a2dba80bb6c03187bf
kh498 <klink100@gmail.com>
2017-07-02 22:57:54 +0200

    Add ways to convert Objects to Ternary

--- src/main/java/no/kh498/util/Ternary.java
+++ src/main/java/no/kh498/util/Ternary.java
@@ -1,7 +1,7 @@
 package no.kh498.util;
 
 /**
- * A three way logic operator.
+ * A three valued logic type
  *
  * @author karl henrik
  */
@@ -30,5 +30,60 @@ public enum Ternary {
      */
     public boolean isKnown() {
         return this != UNKNOWN;
+    }
+
+    /**
+     * Get the corresponding {@code Ternary} state from an integer
+     *
+     * @param i The integer to convert
+     *
+     * @return {@code (i == 1) ? TRUE : ((i == 0) ? FALSE : UNKNOWN)}
+     */
+    public static Ternary get(final int i) {
+        return (i == 1) ? TRUE : ((i == 0) ? FALSE : UNKNOWN);
+    }
+
+    /**
+     * Get the corresponding {@code Ternary} state from a float
+     *
+     * @param f The float to convert to {@code Ternary}
+     *
+     * @return {@code (i == 1) ? TRUE : ((i == 0) ? FALSE : UNKNOWN)}
+     */
+    public static Ternary get(final float f) {
+        return (f == 1f) ? TRUE : ((f == 0f) ? FALSE : UNKNOWN);
+    }
+
+    /**
+     * Get the corresponding {@code Ternary} state from a double
+     *
+     * @param d The double to convert to {@code Ternary}
+     *
+     * @return {@code (i == 1) ? TRUE : ((i == 0) ? FALSE : UNKNOWN)}
+     */
+    public static Ternary get(final double d) {
+        return (d == 1d) ? TRUE : ((d == 0d) ? FALSE : UNKNOWN);
+    }
+
+    /**
+     * Get the corresponding {@code Ternary} state from an integer
+     *
+     * @param b The boolean to convert to {@code Ternary}
+     *
+     * @return {@code b ? TRUE : FALSE}
+     */
+    public static Ternary get(final boolean b) {
+        return b ? TRUE : FALSE;
+    }
+
+    /**
+     * Get the corresponding {@code Ternary} state from an integer
+     *
+     * @param s The boolean to convert to {@code Ternary}, case insensitive
+     *
+     * @return {@code "TRUE".equalsIgnoreCase(s) ? TRUE : ("FALSE".equalsIgnoreCase(s) ? FALSE : UNKNOWN)}
+     */
+    public static Ternary get(final String s) {
+        return "TRUE".equalsIgnoreCase(s) ? TRUE : ("FALSE".equalsIgnoreCase(s) ? FALSE : UNKNOWN);
     }
 }
